adb7d96f0c5ba5fef1f16e79e01609c70b740487
Bolu Akinlawon <78508788+BoluAkinlawon@users.noreply.github.com>
2023-01-27 17:46:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ model = pickle.load(open('random_forest_model.pkl','rb'))
 
 @app.route('/')
 def home():
-    return render_template('templates/index.html')
+    return render_template('template/index.html')
 
 @app.route('/predict', methods =['POST'])
 def predict():
@@ -20,7 +20,7 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
     output = round(prediction[0], 2)
-    return render_template('index.html', prediction_text = 'Price range should be {}'.format(output))
+    return render_template('template/index.html', prediction_text = 'Price range should be {}'.format(output))
 
 if __name__ == "__main__":
     app.run(debug = True)
